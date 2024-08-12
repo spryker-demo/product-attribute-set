eaa1c27a69e130c17e2f4831380dca7a5558144c
MaryamTarekOmar <maryam.tarek@spryker.com>
2024-08-12 14:37:00 +0300

    SLA-6361 adjust attribute sets to accept attributes with empty values inside

--- src/SprykerDemo/Zed/ProductAttributeSet/Business/Reader/ProductAttributeSetReader.php
+++ src/SprykerDemo/Zed/ProductAttributeSet/Business/Reader/ProductAttributeSetReader.php
@@ -7,10 +7,9 @@
 
 namespace SprykerDemo\Zed\ProductAttributeSet\Business\Reader;
 
-use ArrayObject;
 use Generated\Shared\Transfer\ProductAttributeSetCriteriaTransfer;
 use Generated\Shared\Transfer\ProductAttributeSetTransfer;
-use Generated\Shared\Transfer\ProductManagementAttributeFilterTransfer;
+use Generated\Shared\Transfer\ProductManagementAttributeCriteriaTransfer;
 use Spryker\Zed\ProductAttribute\Business\ProductAttributeFacadeInterface;
 use SprykerDemo\Zed\ProductAttributeSet\Persistence\ProductAttributeSetRepositoryInterface;
 
@@ -54,10 +53,7 @@ class ProductAttributeSetReader implements ProductAttributeSetReaderInterface
             return null;
         }
 
-        $productManagementAttributeTransfers = $this->productAttributeFacade->getProductManagementAttributes(
-            new ProductManagementAttributeFilterTransfer(),
-        )->getProductManagementAttributes();
-        $this->addProductManagementAttributesToProductAttributeSet($productAttributeSetTransfer, $productManagementAttributeTransfers);
+        $this->addProductManagementAttributesToProductAttributeSet($productAttributeSetTransfer);
 
         return $productAttributeSetTransfer;
     }
@@ -82,15 +78,17 @@ class ProductAttributeSetReader implements ProductAttributeSetReaderInterface
 
     /**
      * @param \Generated\Shared\Transfer\ProductAttributeSetTransfer $productAttributeSetTransfer
-     * @param \ArrayObject $productManagementAttributeTransfers
      *
      * @return \Generated\Shared\Transfer\ProductAttributeSetTransfer
      */
     protected function addProductManagementAttributesToProductAttributeSet(
-        ProductAttributeSetTransfer $productAttributeSetTransfer,
-        ArrayObject $productManagementAttributeTransfers
+        ProductAttributeSetTransfer $productAttributeSetTransfer
     ): ProductAttributeSetTransfer {
-        foreach ($productManagementAttributeTransfers as $productManagementAttributeTransfer) {
+        $productManagementAttributeCollectionTransfer = $this->productAttributeFacade->getProductManagementAttributeCollection(
+            new ProductManagementAttributeCriteriaTransfer(),
+        );
+
+        foreach ($productManagementAttributeCollectionTransfer->getProductManagementAttributes() as $productManagementAttributeTransfer) {
             if (
                 in_array(
                     $productManagementAttributeTransfer->getIdProductManagementAttribute(),
